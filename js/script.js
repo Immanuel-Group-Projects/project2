0df0f800c5822ec646fb5f0b0887539025d8c3da
cvonk02 <cvonk@hawk.iit.edu>
2023-11-30 19:11:24 -0600

    Update script.js
    Added response query to script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 const sendButton = document.getElementById("send");
 
+function submitForm(){
+    var name = document.forms["contactUs"]["name"].value;
+    var email = document.forms["contactUs"]["email"].value;
+    document.getElementById('notification').play();
+    alert("Thank you for contacting us " + name + ". We will email you at " + email + " when we are available.");
+  }
+
