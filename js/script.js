1d1c4e93e88f6c664690b339c8b9a5cf9a3cb45f
jaanzaldo <jaanzaldo12@gmail.com>
2023-12-05 17:58:24 -0600

    replaced javascript accordion menu with <details> tags

--- js/script.js
+++ js/script.js
@@ -12,17 +12,3 @@ function submitForm(){
     alert("Thank you for adding your bio!");
    });
    
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block"){
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";;
-    }
-  });
-}
