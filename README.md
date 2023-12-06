5a5d513f1bd913840ca0a2827e5bf48d64debdc7
Jorge Anzaldo <jaanzaldo12@gmail.com>
2023-12-05 22:12:56 -0600

    made accordion function
    needed to move script to the bottom instead of in the head

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Jorge Anzaldo's fork of Project 2 for ITMD-362
 Including changes already made that weren't a part of this fork, I'm working on:
 
 additional Javascript for the contact page - In the form of an alert when sending
+
 slideshow for images on events page
+
 making sure whitespace is consistent
+
 replacing details tag with a working accordion
+
 editing media tags to make website more readable on smaller screens
--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ jQuery(function($) {
 	alert('Thank you for reaching out to us. Expect a message within the next few days.');
   });
 });
+
 function checkType() {
   var input = document.getElementById('resume');
   var path = input.value;
@@ -15,4 +16,20 @@ function checkType() {
 	  input.value = '';
 	  return;
   }
+}
+
+var accordion = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+	accordion[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
 }
